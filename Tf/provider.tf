ff4633d5404072bed15609a18a335594e05fd836
Oleksandr Khabarov <okhab@softserveinc.com>
2022-05-03 13:45:08 +0300

    remote state in bucket

--- Tf/provider.tf
+++ Tf/provider.tf
@@ -2,6 +2,12 @@ provider "google" {
   project = "okhab-education-25433"
   region  = "us-east1"
   #credentials = file ("/home/sahay/dangerzone/restricted/firstwebaccess.json")
-  
+}
+
+terraform {
+  backend "gcs" {
+    bucket         = "sahay-tfstate"
+    prefix         = "tgbot/remstate"
+  }
 }
 
